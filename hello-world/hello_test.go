a887a1682a10ace3b5e287a186c754efbebec1bd
Mohammed Hamza <211220035@nitdelhi.ac.in>
2025-04-13 23:01:30 +0530

    Subtests and if

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,12 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Hamza")
-	want := "Hello Hamza"
+	t.Run("say hello to people ", func(t *testing.T) {
+		got := Hello("Hamza")
+		want := "Hello Hamza"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if(got != want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 
+	t.Run("saying Hello World when emoty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+
+		if(got != want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
 
